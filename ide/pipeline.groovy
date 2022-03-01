75cb8257ee3e039e73d6af8973f8f82a6203bbb6
Kanth81 <lakshmikanthtn@gmail.com>
2022-03-01 02:20:24 +0000

    update pipeline groovy

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,21 +1,30 @@
 pipeline {
     agent any
-
+    triggers { pollSCM('* * * * *') }
     stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/Kanth81/course-jenkins-getting-started.git', branch: 'main'
+            }            
+        }
         stage('Build') {
             steps {
-                
-                git url: 'https://github.com/Kanth81/jgsu-spring-petclinic.git', branch: 'main'
-                sh './mvnw clean compile'
-              
+                sh './mvnw clean package'
+                //sh 'false' // true
             }
-
+        
             post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
+                always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                }
+                changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build", 
+                        attachLog: true, 
+                        compressLog: true, 
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
