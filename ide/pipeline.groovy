4f6b90d4940d7d5f66d5c7dddbeb24f6aafa9418
Kanth81 <lakshmikanthtn@gmail.com>
2022-03-01 02:56:13 +0000

    pipeline groovy update

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,30 +1,19 @@
 pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
-        stage('Checkout') {
-            steps {
-                git branch: 'main', url: 'https://github.com/Kanth81/jgsu-spring-petclinic.git'
-            }            
-        }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/Kanth81/jgsu-spring-petclinic.git'
+
+               sh './mvnw clean compile'
             }
-        
+
             post {
-                always {
+                    always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
